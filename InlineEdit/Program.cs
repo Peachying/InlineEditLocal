68e4bb992aa5cee9f420ace18132682679dd64ea
Yuchang Xu <t-yucxu@microsoft.com>
2019-06-28 18:29:00 +0800

    modify the origin md file

--- InlineEdit/Program.cs
+++ InlineEdit/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace InlineEdit
@@ -7,22 +9,29 @@ namespace InlineEdit
     {
         static void Main(string[] args)
         {
-            string path = @"C:\Users\t-yucxu\Desktop\testInlineEdit\middlefile";            
+            string path = @"C:\Users\t-yucxu\Desktop\testInlineEdit\middlefile";
             string head = @"pandoc -f html -t markdown ";
             string tail = @" -o C:\Users\t-yucxu\Desktop\testInlineEdit\middlefile\frag_";
-            
+
             System.Diagnostics.Process p = new System.Diagnostics.Process();
             p.StartInfo.FileName = "cmd.exe";
-            p.StartInfo.UseShellExecute = false;    
+            p.StartInfo.UseShellExecute = false;
             p.StartInfo.RedirectStandardInput = true;
             p.StartInfo.RedirectStandardOutput = true;
             p.StartInfo.RedirectStandardError = true;
             p.StartInfo.CreateNoWindow = true;
-            p.Start();            
+            p.Start();
 
             //DirectoryInfo dir = new DirectoryInfo(path);
             //FileInfo[] f = dir.GetFiles();
             string[] filenames = Directory.GetFiles(path, "*.html");
+            StreamReader sr = new StreamReader(path + @"\editinfo.txt");
+            Origin_info origin_info = JsonConvert.DeserializeObject<Origin_info>(@"{"startline":"16","endline":"16","origin_url":"https://github.com/Peachying/testinlineedit/blob/master/node-azure-tools.md"}");
+            string json = sr.ReadToEnd();
+            foreach (string jn in json)
+            {
+                Origin_info origin_info = JsonConvert.DeserializeObject<Origin_info>(jn);
+            }
             foreach (string file in filenames)
             {
                 string exec = head + file + tail + file.Substring(file.IndexOf("_") + 1, 1) + ".md\n";
